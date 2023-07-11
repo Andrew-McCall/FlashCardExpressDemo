84b2e4a8a45da1b8b25151e723001110f66a359c
Andrew-McCall <amccall@qa.com>
2023-07-11 18:29:11 +0100

    SEt Extra functions

--- routes/setCRUD.js
+++ routes/setCRUD.js
@@ -11,8 +11,8 @@ router.get("/", (req, res) => {
     PATCH    /set/update/:set_id            Body:{set_name:String, card_ids:[ ObjectIds ]}
     DELETE   /set/delete/:set_id            Body:N/A
 
-    POST     /set/AddCard/:set_id/:card_id  Body:N/A
-    POST     /set/AddCards/:set_id          Body:{card_ids:[ ObjectIds ]}
+    POST     /set/addCard/:set_id/:card_id  Body:N/A
+    POST     /set/addNewCard/:set_id        Body:{front_text:String, back_text:String}
     `
     res.status(418).send(help)
 })
@@ -62,12 +62,51 @@ router.patch("/update/:id", (req,res,error) => {
 router.post("/delete/:id", (req, res, error) => {
     // Find and delete set
     setModel.findByIdAndDelete(req.params.id).then( set => {
-        // Delete all cards in that set
-        cardModel.deleteMany({_id:{$in:set.card_ids}}).then(() => {
-            // Respond
-            res.status(200).json(set)
+        // Respond
+        res.status(200).json(set)
+    }).catch(error)
+})
+
+router.post("/set/addCard/:set_id/:card_id", (req, res, error) => {
+    // Find set
+    setModel.findById(req.params.set_id).then( set => {
+
+        // Find card (to verify it exists)
+        cardModel.findById(req.params.card_id).then( card => {
+            
+            // Check if it's already in set
+            if (set.card_ids.indexOf(card._id) > -1){
+                throw Error("Set Already has that card!")
+            }
+
+            // Add to set
+            set.card_ids.push(card)
+
+            // Save set and respond
+            set.save().populate("card_ids").then( populatedSet => {
+                res.status(200).json(populatedSet)
+            })
+
+        })
+
+    }).catch(error)
+})
+
+router.post("/set/addNewCard/:id", (req, res, error) => {
+
+    // Create new card
+    cardModel.create(req.body).then( newCard => {
+        setModel.findById(req.params.id).then(set => {
+            // Add new card to set
+            set.card_ids.push(newCard)
+
+            // Save and Respond
+            set.save().populate("card_ids").then( populatedSet => {
+                res.status(200).json(populatedSet)
+            })
         })
     }).catch(error)
+
 })
 
 module.exports = router
